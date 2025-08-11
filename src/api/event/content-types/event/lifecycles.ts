c8ed4971ead118c180398835fa415189a6f116f2
algnot <thanawat.tongla@gmail.com>
2025-08-12 00:11:08 +0700

    Enhance TypeScript configuration and update schema definitions for improved clarity and functionality. Added source maps to tsconfig.json, standardized attribute names in schema.json, and updated generated TypeScript definitions for consistency.

---
+++ src/api/event/content-types/event/lifecycles.ts
@@ -0,0 +1,25 @@
+
+import { errors } from '@strapi/utils';
+const { ApplicationError } = errors;
+
+export default {
+    async beforeCreate(event) {
+        const { data } = event.params;
+
+        if (data.title) {
+            data.title = data.title.trim();
+        }
+
+        if (!data.slug && data.title) {
+            const slug = data.title.toLowerCase().replace(/\s+/g, '-');
+
+            const { results } = await strapi.service('api::event.event').find({ filters: { slug } });
+
+            if (Array.isArray(results) && results.length > 0) {
+                data.slug = data.title.toLowerCase().replace(/\s+/g, '-') + '-' + results.length;
+            } else {
+                data.slug = data.title.toLowerCase().replace(/\s+/g, '-')
+            }
+        }
+    },
+};
