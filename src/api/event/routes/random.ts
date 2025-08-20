23c53e901d4ea5084697ae563ddbf436ddcd3a14
algnot <thanawat.tongla@gmail.com>
2025-08-20 23:48:54 +0700

    feat: add random endpoint

--- src/api/event/routes/random.ts
+++ src/api/event/routes/random.ts
@@ -2,6 +2,14 @@ export default {
     routes: [
         {
             method: "GET",
+            path: "/events",
+            handler: "event.find",
+            config: {
+                auth: false,
+            },
+        },
+        {
+            method: "GET",
             path: "/events/random",
             handler: "event.random",
             config: {
--- src/api/newsroom/routes/random.ts
+++ src/api/newsroom/routes/random.ts
@@ -2,6 +2,14 @@ export default {
     routes: [
         {
             method: "GET",
+            path: "/newsrooms",
+            handler: "event.find",
+            config: {
+                auth: false,
+            },
+        },
+        {
+            method: "GET",
             path: "/newsrooms/random",
             handler: "newsroom.random",
             config: {
